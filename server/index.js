c8855e61f65cd64244143ffcb286c6ce316b2eda
allebd <delebella@yahoo.com>
2019-05-27 14:45:26 +0100

    CH-166267668-task (setup): setup node and express
    Update port environment
    
    [Finishes CH-166267668]

--- server/index.js
+++ server/index.js
@@ -2,15 +2,30 @@ import { config } from 'dotenv';
 import express from 'express';
 
 config();
-const port = process.env.PORT; // setup port to be used
+const port = process.env.PORT || 5000; // setup port to be used
 const app = express(); // calling an instance of express
 
 app.get('/', (request, response) => {
     try{
         response.status(200).send('Hello Books');
     } catch (error){
-        response.status(404).send('Error: Address not found');  
+        response.status(500).send('Server error');  
     }
+});
+
+app.use('/*', (request, response) => {
+    try{
+        response.status(404).json({
+            status: 404,
+            message: 'Address not found!',
+        });
+    } catch (error){
+        response.status(500).json({
+            status: 500,
+            message: 'Server Error',
+        });  
+    }
+    
 });
 
 app.listen(port, () => {
