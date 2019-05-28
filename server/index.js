abaab01c633a0199671a5463d1da9b711081792a
Adeola Adeyemo <adeoladeyemo@gmail.com>
2019-05-28 20:37:16 +0100

    ch-166214083-task(swaggerSetup): added swagger document installed swagger ui created and added swagger document [finishes #166214083]

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ import { config } from 'dotenv';
 import logger from 'morgan';
 import validator from 'express-validator';
 import cors from 'cors';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
 
 config();
 
@@ -21,8 +23,12 @@ app.get('/', (request, response) => {
     response.status(200).send('Hello Books');
 });
 
+//load swagger documentation
+swaggerDocument.servers[0].url = `http://localhost:${PORT}`;
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use('*', (request, response) => {
-    response.status(404).send('Not Found');
+    response.status(404).send('Not Found'); 
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
