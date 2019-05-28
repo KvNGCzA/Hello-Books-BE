99fffc9aa8f3fed5828095f7ba5422f932fc21c6
Lundii <mondayemmanuel67@gmail.com>
2019-05-28 14:34:48 +0100

    166214087-chore(backend): setup prettier
    installed pretty-quick to repo
    
    [Finishes #166214087]

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 import express, { json, urlencoded } from 'express';
 import { config } from 'dotenv';
 import logger from 'morgan';
@@ -12,17 +11,21 @@ const app = express(); // calling an instance of express
 
 app.use(logger('dev'));
 app.use(json());
-app.use(urlencoded({ extended: false }));
+app.use(
+  urlencoded({
+    extended: false
+  })
+);
 app.use(validator());
 app.use(cors());
 
 // index route
 app.get('/', (request, response) => {
-    response.status(200).send('Hello Books');
+  response.status(200).send('Hello Books');
 });
 
 app.use('*', (request, response) => {
-    response.status(404).send('Not Found');
+  response.status(404).send('Not Found');
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
