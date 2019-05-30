d4c021d68187139229b1f535fb49eda671e8adb2
allebd <delebella@yahoo.com>
2019-05-30 19:17:02 +0100

    166313092-story(users model):create user model
    create user model
    
    [Finishes #166313092]

---
+++ server/database/models/users.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Users = sequelize.define('Users', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    avatarUrl: DataTypes.STRING,
+    verified: DataTypes.BOOLEAN
+  }, {});
+  Users.associate = function(models) {
+    // associations can be defined here
+  };
+  return Users;
+};
