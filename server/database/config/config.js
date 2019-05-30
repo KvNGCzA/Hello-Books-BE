b3140500436a032c091b73be07604a76b054e1e1
oluwajhay <lordsylharx@gmail.com>
2019-05-30 18:38:16 +0100

    166313415-story (model): roles model
    -create roles model
    -create admin seeder file
    -create patron seeder file
    
    [Finishes #166313415]

--- server/database/config/config.js
+++ server/database/config/config.js
@@ -1,23 +1,24 @@
+/* eslint-disable */
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_DEV,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    dialect: 'postgres'
-  },
-  test: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_TEST,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    dialect: 'postgres'
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL'
-  }
+    development: {
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_DEV,
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        dialect: 'postgres'
+    },
+    test: {
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_TEST,
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        dialect: 'postgres'
+    },
+    production: {
+        use_env_variable: 'DATABASE_URL'
+    }
 };
---
+++ server/database/models/roles.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+module.exports = (sequelize, DataTypes) => {
+    const Roles = sequelize.define('Roles', {
+        roleName: DataTypes.STRING,
+        description: DataTypes.STRING
+    }, {});
+    Roles.associate = function(models) {
+        // associations can be defined here
+    };
+    return Roles;
+};
