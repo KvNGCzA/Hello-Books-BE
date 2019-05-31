af20b2d57e0357e548ac2b859e11a731cbafba58
oluwajhay <lordsylharx@gmail.com>
2019-05-31 11:39:26 +0100

    166313415-story(model): create roles model
    -create roles model
    -create admin role seeder
    -create patron role seeder
    
    [Finishes #166313415]

---
+++ server/database/seeders/20190529165248-roles.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+module.exports = {
+    up: (queryInterface, Sequelize) => queryInterface.bulkInsert('Roles', [{
+        roleName: 'admin',
+        description: 'Administrator of the library',
+        createdAt: new Date(),
+        updatedAt: new Date()
+    }, {
+        roleName: 'patron',
+        description: 'A full supporter of the library',
+        createdAt: new Date(),
+        updatedAt: new Date()
+    }], {}),
+
+    down: (queryInterface, Sequelize) => queryInterface.bulkDelete('Roles', null, {})
+
+};
