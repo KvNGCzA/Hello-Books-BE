e06fd831dead89a05327745b68e53bfb06418b71
oluwajhay <lordsylharx@gmail.com>
2019-06-04 08:51:34 +0100

    FT-166214098-story(user): verify email address
    - a user should be able to verify email after signup
    
    [Finishes #166214098]

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -10,12 +10,12 @@ export default (request, response, next) => {
   const token = request.headers.authorization || request.query.token;
 
   if (token) {
-    jwt.verify(token, process.env.JWT_KEY, (error, decoded) => {
+    jwt.verify(token, process.env.JWT_KEY, async(error, decoded) => {
       if (error) {
         const message = (error.name === 'TokenExpiredError') ? 'token expired' : 'invalid token';
         responseMessage(response, 401, { message });
       } else {
-        const user = findUser(decoded.id);
+        const user = await findUser(decoded.id);
         if (!user) return responseMessage(response, 404, { message: 'user not found' });
         request.userData = user;
         next();
