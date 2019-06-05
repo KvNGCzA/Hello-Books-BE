ac5b0f9e21eb4277b2923ab14ecabd528e8c0a2c
Adekoreday <khord4eng@gmail.com>
2019-06-05 08:53:10 +0100

    #166214096 story(signUp Mail): User receive Mail upon sucessfull registration
    User receive mail to confirm  registration
    
    [Delivers #166214096]

--- server/helpers/index.js
+++ server/helpers/index.js
@@ -3,11 +3,15 @@ import responseMessage from './responseMessage';
 import sendMail from './sendMail';
 import findUser from './findUser';
 import makeLowerCase from './customSanitizers';
+import * as messages from './messages';
 
+const { signupMessage, siginMessage } = messages;
 export default {
   createToken,
   responseMessage,
   findUser,
   sendMail,
   makeLowerCase,
+  signupMessage,
+  siginMessage,
 };
