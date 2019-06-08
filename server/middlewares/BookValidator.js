68e4a703999a315f4f6cb4fbc361e2bb93019fab
Adeola Adeyemo <adeoladeyemo@gmail.com>
2019-06-08 21:14:52 +0100

    166557848-story(bookValidator): Implement Book Validation Middleware [Starts #166557848]

---
+++ server/middlewares/BookValidator.js
@@ -0,0 +1,116 @@
+import UserValidator from './UserValidator';
+import checkForErrors from './checkForErrors';
+
+const year = new Date().getFullYear();
+
+/**
+ * @class BookValidator
+ * @classdesc Validates the book addition route
+ */
+export default class BookValidator {
+  /**
+   * Title validator
+   * @returns {function} call to a check API middleware
+   * @memberof Validation
+   */
+  static checkTitle() {
+    return UserValidator.genericCheck('title')
+      .trim()
+      .isLength({ min: 2, max: 50 })
+      .withMessage('title must be at least 2 characters, and maximum 50')
+      .not()
+      .matches(/^[0-9]+([,.][0-9]+)?$/, 'g')
+      .withMessage('title must contain alphabets')
+      .not()
+      .matches(/^[^a-zA-Z0-9]+$/, 'g')
+      .withMessage('title must contain alphabets');
+  }
+
+  /**
+   * Description validator
+   * @returns {function} call to a check API middleware
+   * @memberof Validation
+   */
+  static checkDescription() {
+    return UserValidator.genericCheck('description')
+      .trim()
+      .isLength({ min: 10 })
+      .withMessage('description must be at least 10 characters')
+      .not()
+      .matches(/^[0-9]+([,.][0-9]+)?$/, 'g')
+      .withMessage('description must contain alphabets')
+      .not()
+      .matches(/^[^a-zA-Z0-9]+$/, 'g')
+      .withMessage('description must contain alphabets');
+  }
+
+  /**
+   * Isbn validator
+   * @returns {function} call to a check API middleware
+   * @memberof Validation
+   */
+  static checkIsbn() {
+    return UserValidator.genericCheck('isbn')
+      .trim()
+      .isISBN()
+      .withMessage('isbn is not valid');
+  }
+
+  /**
+   * Price validator
+   * @returns {function} call to a check API middleware
+   * @memberof Validation
+   */
+  static checkPrice() {
+    return UserValidator.genericCheck('price')
+      .trim()
+      .isCurrency({
+        symbol: 'N',
+        allow_negatives: false,
+      })
+      .withMessage('price should be a valid currency amount');
+  }
+
+  /**
+   * Year published validator
+   * @returns {function} call to a check API middleware
+   * @memberof Validation
+   */
+  static checkYearPublished() {
+    return UserValidator.genericCheck('yearPublished')
+      .trim()
+      .isInt({ min: 1455, max: year })
+      .withMessage('year published must be a valid year');
+  }
+
+  /**
+   * Generic Number validator
+   * @param {string} item
+   * @returns {function} call to a check API middleware
+   * @memberof Validation
+   */
+  static checkNumber(item) {
+    return UserValidator.genericCheck(item)
+      .trim()
+      .isInt({ min: 1 })
+      .withMessage(`${item} value must be at least 1 and an integer`);
+  }
+
+  /**
+   * Book validator
+   * @returns {array} an array of check API middleware
+   * @memberof Validation
+   */
+  static BookValidation() {
+    return [
+      BookValidator.checkTitle(),
+      BookValidator.checkDescription(),
+      BookValidator.checkIsbn(),
+      BookValidator.checkPrice(),
+      BookValidator.checkYearPublished(),
+      BookValidator.checkNumber('stock'),
+      BookValidator.checkNumber('authorId'),
+      checkForErrors,
+    ];
+  }
+}
